39ac85549f4259b990211c064ab7cf8e0942645f
hakobtam <tam.hak27@gmail.com>
2017-05-07 21:50:29 +0400

    changeing runner.py

--- runner.py
+++ runner.py
@@ -41,7 +41,7 @@ def gradient_descent(X, Y, epsilon=1e-6, l=1, step_size=1e-4, max_steps=1000):
 def evaluate_performance():
     '''
     Evaluate the performance of decision trees and logistic regression,
-    average over 1,000 trials of 10-fold cross validation
+    average over 1,000 trials of 10-folds cross validation
 
     Return:
       a matrix giving the performance that will contain the following entries:
@@ -54,6 +54,7 @@ def evaluate_performance():
     '''
 
     # Load Data
+    folds = 5
     filename = 'SPECTF.dat'
     data = np.loadtxt(filename, delimiter=',')
     X = data[:, 1:]
@@ -66,36 +67,46 @@ def evaluate_performance():
         idx = np.arange(n)
         np.random.seed(13)
         np.random.shuffle(idx)
+
         X = X[idx]
         y = y[idx]
 
-        Xtrain = X[:101, :]
-        Xtest = X[101:, :]
-        ytrain = y[:101, :]
-        ytest = y[101:, :]
+        tree_acc = []
+        forest_acc = []
+        log_acc = []
+        for it in range(folds):
+            Xtest = X[it::folds, :]
+            ytest = y[it::folds, :]
+    
+            Xtrain = [X[i] for i in range(len(X)) if i % folds != it]
+            ytrain = [y[i] for i in range(len(y)) if i % folds != it]
 
-        classifier = DecisionTree(3)
-        classifier.fit(Xtrain, ytrain)
+            classifier = DecisionTree(3)
+            classifier.fit(Xtrain, ytrain)
 
-        y_pred = classifier.predict(Xtest)
-        tree_accuracies.append(accuracy_score(ytest, y_pred))
-        #print("acc of decision tree= ", accuracy_score(ytest, y_pred))
+            y_pred = classifier.predict(Xtest)
+            tree_acc.append(accuracy_score(ytest, y_pred))
+            #print("acc of decision tree= ", accuracy_score(ytest, y_pred))
 
-        classifier = RandomForest(27,100)
-        classifier.fit(Xtrain, ytrain)
+            classifier = RandomForest(27,100)
+            classifier.fit(Xtrain, ytrain)
 
-        y_pred = classifier.predict(Xtest)[0]
-        forest_accuracies.append(accuracy_score(ytest, y_pred))
-        #print("acc of forest= ", accuracy_score(ytest, y_pred))
+            y_pred = classifier.predict(Xtest)[0]
+            forest_acc.append(accuracy_score(ytest, y_pred))
+            #print("acc of forest= ", accuracy_score(ytest, y_pred))
 
-        dd = np.array(Xtrain)
-        data_train = np.column_stack((np.ones(dd.shape[0]), dd))
-        label_train = [-1 if a == 0 else 1 for a in ytrain]
-        beta_hat = gradient_descent(data_train, label_train, epsilon=1e-3, l=1, step_size=0.1, max_steps=200)
+            dd = np.array(Xtrain)
+            data_train = np.column_stack((np.ones(dd.shape[0]), dd))
+            label_train = [-1 if a == 0 else 1 for a in ytrain]
+            beta_hat = gradient_descent(data_train, label_train, epsilon=1e-3, l=1, step_size=0.1, max_steps=200)
         
-        y_pred = [1 if a >= 0 else -1 for a in data_train.dot(beta_hat)]
-        log_accuracies.append(accuracy_score(label_train, y_pred))
-        #print("acc of logistic= ", accuracy_score(label_train, y_pred))
+            y_pred = [1 if a >= 0 else -1 for a in data_train.dot(beta_hat)]
+            log_acc.append(accuracy_score(label_train, y_pred))
+            #print("acc of logistic= ", accuracy_score(label_train, y_pred))
+
+        tree_accuracies.append(np.mean(tree_acc))
+        forest_accuracies.append(np.mean(forest_acc))
+        log_accuracies.append(np.mean(log_acc))
 
     # compute the training accuracy of the model
     meanDecisionTreeAccuracy = np.mean(tree_accuracies)
