d56029a2c48c637ca2a822538705dcbc5cd728df
hakobtam <tam.hak27@gmail.com>
2017-05-08 12:29:58 +0400

    separating logistic from runner.py file

--- runner.py
+++ runner.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random_forest import RandomForest
 from decision_tree import DecisionTree
-
+from logistic_regression import gradient_descent
 
 def accuracy_score(Y_true, Y_predict):
     acc = 0.
@@ -10,33 +10,6 @@ def accuracy_score(Y_true, Y_predict):
         #print(y_t, ' ', y_p)
         acc += (y_t == y_p)
     return acc * 100. / len(Y_true)
-
-def sigmoid(s):
-    return 1.0/(1.0 + np.exp(-s))
-
-def normalized_gradient(X, Y, beta, l):
-    return np.sum(np.array([-Y[i] * X[i] * (1 - sigmoid(Y[i]*(beta.T.dot(X[i])))) for i in range(len(Y))]), axis = 0)/len(Y) + l * beta/len(Y)
-
-def gradient_descent(X, Y, epsilon=1e-6, l=1, step_size=1e-4, max_steps=1000):
-
-    beta = np.zeros(X.shape[1])
-    mean_val = np.mean(X[:,1:], axis = 0)
-    sigma = np.std(X[:,1:], axis = 0) 
-    lam = np.hstack((0, l / (sigma ** 2)))
-    mean_val = np.hstack((0, mean_val))
-    sigma = np.hstack((1, sigma))
-    X_scale = (X - mean_val) / sigma
-    step_size = 0.1
-    beta = np.zeros((X.shape[1]))
-    for s in range(max_steps):
-        grad = normalized_gradient(X_scale, Y, beta, lam)
-        beta = beta - step_size * grad
-        if np.linalg.norm(step_size * grad) / np.linalg.norm(beta) < epsilon:
-            break
-    beta[0] = beta[0] - np.sum((mean_val * beta) / sigma)
-    beta[1:] = beta[1:] / sigma[1:]
-    return beta
-
 
 def evaluate_performance():
     '''
@@ -55,7 +28,7 @@ def evaluate_performance():
 
     # Load Data
     folds = 5
-    filename = 'data/SPECTF.dat'
+    filename = 'SPECTF.dat'
     data = np.loadtxt(filename, delimiter=',')
     X = data[:, 1:]
     y = np.array([data[:, 0]]).T
